ca9d55614c84d9c71b308d5020fe5a27b84cb2d9
Muhammad Umar <Muhammadumar695@gmail.com>
2024-11-17 22:34:41 +0500

    feat: Add example C++ program demonstrating the use of functions

--- 5. Functions in C++/1. Basics of Functions in C++/5_Example.cpp
+++ 5. Functions in C++/1. Basics of Functions in C++/5_Example.cpp
@@ -1,20 +1,23 @@
 
-#include<iostream>
-#include<conio.h>
+// Program in C++ to demonstrate the use of functions in C++
 
-using namespace std;
+#include<iostream> 
+#include<conio.h>  
 
-int xyz( int input) {
-    cout << "Enter the input please ( Taking input inside function ) : ";
-	cin >> input;
-	return input;
+using namespace std;  
+
+// Function to take input from the user
+int xyz(int input) {
+    cout << "Enter the input please ( Taking input inside function ) : ";  // Prompt the user for input
+    cin >> input;  // Read the input from the user
+    return input;  // Return the input value
 }
 
 int main() {
+    int output = 0;  // Initialize output variable to 0
+	// Call the xyz function and display the returned value
+    cout << "Displying the output ( In the main() body ) : " << xyz(output);  
 
-	int output = 0;
-	cout << "Displying the output ( In the main() body ) : " << xyz(output);
-
-	_getch();
-	return 0;
+    _getch(); 
+    return 0;  
 }
